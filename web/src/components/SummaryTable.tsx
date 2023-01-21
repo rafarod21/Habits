32f7b418b5449f7f5fae59aba83013454d6b49b6
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2023-01-21 12:18:25 -0300

    Aula 2

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,41 @@
+import { HabitDay } from './HabitDay';
+
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimunSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minimunSummaryDatesSize - summaryDates.length;
+
+export function SummaryTable() {
+  return (
+    <div className='w-full flex '>
+      <div className='grid grid-rows-7 grid-flow-row gap-3'>
+        {weekDays.map((weekDay, index) => (
+          <div
+            key={`${weekDay}-${index}`}
+            className='text-zinc-400 text-xl h-10 w-10 flex items-center justify-center font-bold'
+          >
+            {weekDay}
+          </div>
+        ))}
+      </div>
+
+      <div className='grid grid-rows-7 grid-flow-col gap-3'>
+        {summaryDates.map((date) => (
+          <HabitDay key={date.toString()} />
+        ))}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((value, index) => (
+            <div
+              key={index}
+              className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed'
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
